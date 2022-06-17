3e187795e95d3cdd40955bf1cf94fc513315e51f
unnati1004 <95961486+unnati1004@users.noreply.github.com>
2022-06-18 01:42:35 +0530

    update

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -3,11 +3,13 @@ import { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const alphabet = "abcdefghijklmnopqrstuvwxyz"
-  const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)]
+  const letter =()=>{
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+    return alphabet[Math.floor(Math.random() * alphabet.length)]
+  }
 useEffect(()=>{
 document.addEventListener("keydown",detectkey)
-})
+const a = letter();
 const detectkey=(e)=>{
   // console.log(e.key);
   if(e.key===randomCharacter)
@@ -15,9 +17,10 @@ const detectkey=(e)=>{
     console.log(randomCharacter,e.key)
   }
 }
+})
   return (
     <div className="App">
-      <input type="text" value={randomCharacter} className="ran" />
+      <input type="text" onChange={letter}  className="ran" />
        <p>Timer:{}</p>
     </div>
   );
